9fa4f4a8c7c66b6c2cf40ef3940314710a7eb074
Alonesniperr <145885211+Alonesniperr@users.noreply.github.com>
2025-05-18 20:16:11 +0530

    Add files via upload

---
+++ responsive/index.js
@@ -0,0 +1,9 @@
+hamburger = document.querySelector('.hamburger')
+ull = document.querySelector('.ull')
+
+
+hamburger.addEventListener('click' , ()=>{
+  ull.classList.toggle('v-nav')
+navv.classList.toggle('h-nav')
+})
+
